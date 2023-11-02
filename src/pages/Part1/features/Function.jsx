eb6a3a590b80cddb381792c70409646e0921cc30
Castro-1 <jecastrog@eafit.edu.co>
2023-11-02 07:57:46 -0500

    purple function

--- src/pages/Part1/features/Function.jsx
+++ src/pages/Part1/features/Function.jsx
@@ -1,111 +1,112 @@
 import { useRef, useEffect } from "react";
 import {
-    Chart,
-    CategoryScale,
-    LinearScale,
-    LineController,
-    PointElement,
-    LineElement,
-    Title,
+  Chart,
+  CategoryScale,
+  LinearScale,
+  LineController,
+  PointElement,
+  LineElement,
+  Title,
 } from "chart.js";
 import * as math from "mathjs";
 
 Chart.register(
-    CategoryScale,
-    LinearScale,
-    LineController,
-    PointElement,
-    LineElement,
-    Title
+  CategoryScale,
+  LinearScale,
+  LineController,
+  PointElement,
+  LineElement,
+  Title
 );
 
-export default function Function({ expression, method, color="green" }) {
-    const chartRef = useRef(null);
-    const pointRadiusArray = useRef([]);
-    const chartInstance = useRef(null);
+export default function Function({ expression, method, color = "#646cff" }) {
+  const chartRef = useRef(null);
+  const pointRadiusArray = useRef([]);
+  const chartInstance = useRef(null);
 
-    const parseFunction = (expression, minX, maxX, step) => {
-        if (expression.includes("ln")) {
-            expression = expression.replace(/ln\(([^)]+)\)/g, "log($1)*2.303");
-        }
-        const scope = { x: math.range(minX, maxX, step).toArray() };
-        const compiled = math.compile(expression);
-        return scope.x.map((x) => compiled.evaluate({ x }));
+  const parseFunction = (expression, minX, maxX, step) => {
+    if (expression.includes("ln")) {
+      expression = expression.replace(/ln\(([^)]+)\)/g, "log($1)*2.303");
+    }
+    const scope = { x: math.range(minX, maxX, step).toArray() };
+    const compiled = math.compile(expression);
+    return scope.x.map((x) => compiled.evaluate({ x }));
+  };
+
+  useEffect(() => {
+    if (!expression) {
+      alert("Please enter a function.");
+      return;
+    }
+
+    const minX =
+      expression.includes("log") || expression.includes("ln") ? 0 : -20;
+    const maxX = 20;
+    const step = 0.1;
+
+    const data = {
+      labels: math.range(minX, maxX, step).toArray(),
+      datasets: [
+        {
+          label: expression,
+          data: parseFunction(expression, minX, maxX, step),
+          fill: true,
+          borderColor: color,
+          pointRadius: pointRadiusArray.current,
+        },
+      ],
     };
 
-    useEffect(() => {
-        if (!expression) {
-            alert("Please enter a function.");
-            return;
-        }
+    const options = {
+      scales: {
+        x: {
+          type: "linear",
+          position: "bottom",
+        },
+        y: {
+          type: "linear",
+          position: "left",
+        },
+      },
+      plugins: {
+        title: {
+          display: true,
+          text: method + " Chart",
+        },
+        tooltip: {
+          enabled: true,
+          custom: (tooltipModel) => {
+            const { body } = tooltipModel;
+            if (body) {
+              const [label] = body;
+              const [xValue, yValue] = label.lines[0].split(":");
+              return `(${xValue.trim()}, ${yValue.trim()})`;
+            }
+            return "";
+          },
+        },
+      },
+    };
 
-        const minX = expression.includes("log") || expression.includes("ln") ? 0 : -20;
-        const maxX = 20;
-        const step = 0.1;
+    if (chartInstance.current) {
+      chartInstance.current.destroy();
+    }
 
-        const data = {
-            labels: math.range(minX, maxX, step).toArray(),
-            datasets: [
-                {
-                    label: expression,
-                    data: parseFunction(expression, minX, maxX, step),
-                    fill: true,
-                    borderColor: color,
-                    pointRadius: pointRadiusArray.current,
-                },
-            ],
-        };
+    if (chartRef.current) {
+      pointRadiusArray.current = Array(data.labels.length).fill(0);
 
-        const options = {
-            scales: {
-                x: {
-                    type: "linear",
-                    position: "bottom",
-                },
-                y: {
-                    type: "linear",
-                    position: "left",
-                },
-            },
-            plugins: {
-                title: {
-                    display: true,
-                    text: method + " Chart",
-                },
-                tooltip: {
-                    enabled: true,
-                    custom: (tooltipModel) => {
-                        const { body } = tooltipModel;
-                        if (body) {
-                            const [label] = body;
-                            const [xValue, yValue] = label.lines[0].split(":");
-                            return `(${xValue.trim()}, ${yValue.trim()})`;
-                        }
-                        return "";
-                    },
-                },
-            },
-        };
+      const chart = new Chart(chartRef.current, {
+        type: "line",
+        data: data,
+        options: options,
+      });
+      chartInstance.current = chart;
+    }
+  }, [expression, method]);
 
-        if (chartInstance.current) {
-            chartInstance.current.destroy();
-        }
-
-        if (chartRef.current) {
-            pointRadiusArray.current = Array(data.labels.length).fill(0);
-
-            const chart = new Chart(chartRef.current, {
-                type: "line",
-                data: data,
-                options: options,
-            });
-            chartInstance.current = chart;
-        }
-    }, [expression, method]);
-
-    return (
-        <div>
-            <canvas id={`my${method}`} ref={chartRef}></canvas>
-        </div>
-    );
+  return (
+    <div>
+      <canvas id={`my${method}`} ref={chartRef}></canvas>
+    </div>
+  );
 }
